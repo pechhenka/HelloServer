34af2cb8f31daa0d6b46af71f7286a0bf6f87d83
Pavel Sharaev <mail@pechhenka.ru>
2022-10-14 21:34:27 +0300

    add project

---
+++ src/hello/HelloClient.java
@@ -0,0 +1,15 @@
+package hello;
+
+public interface HelloClient {
+    /**
+     * Runs Hello client.
+     * This method should return when all requests are completed.
+     *
+     * @param host     server host
+     * @param port     server port
+     * @param prefix   request prefix
+     * @param threads  number of request threads
+     * @param requests number of requests per thread.
+     */
+    void run(String host, int port, String prefix, int threads, int requests);
+}
---
+++ src/hello/HelloServer.java
@@ -0,0 +1,18 @@
+package hello;
+
+public interface HelloServer extends AutoCloseable {
+    /**
+     * Starts a new Hello server.
+     * This method should return immediately.
+     *
+     * @param port    server port.
+     * @param threads number of working threads.
+     */
+    void start(int port, int threads);
+
+    /**
+     * Stops server and deallocates all resources.
+     */
+    @Override
+    void close();
+}
